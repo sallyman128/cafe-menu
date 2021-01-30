3297ae6c2953b468945e42ff37185368f0891400
Salmaan <salmaan.arif.ali@gmail.com>
2021-01-29 21:34:22 -0500

    successfully hid api-key

--- lib/todays_weather/weather_api.rb
+++ lib/todays_weather/weather_api.rb
@@ -4,7 +4,7 @@ class TodaysWeather::OpenWeatherAPI
 
   # extracts a hash of weather details from API
   def self.weather_data_getter(zip, units)
-    api_key = File.open('.env').read()
+    api_key = open('.env').read().strip
     url = "https://api.openweathermap.org/data/2.5/weather?zip=#{zip}&units=#{units}&appid=#{api_key}"
     uri = URI.parse(url)
     response = Net::HTTP.get(uri)
